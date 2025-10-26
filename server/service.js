84759e2fff6d59e66ab268e2b4bc97aed8450c79
Kurt Rowley <bensonro@byu.edu>
2025-10-25 19:32:14 -0600

    added docs and Http header control

--- server/service.js
+++ server/service.js
@@ -4,8 +4,24 @@ const { dealRouter } = require("./routes/deal.js");
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 const apiRouter = express.Router();
 app.use("/api", apiRouter);
+
+apiRouter.use("/docs", (req, res) => {
+  res.json({
+    version: version.version,
+    endpoints: [...dealRouter.endpoints],
+  });
+});
+
 apiRouter.use("/deal", dealRouter);
 
 app.get("/", (req, res) => {
